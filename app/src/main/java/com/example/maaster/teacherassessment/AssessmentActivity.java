e4afa2a7b9aaa1e4f0a4e9ab55d49356c29d14bc
Pannawat <goku.pun@hotmail.com>
2016-11-29 01:46:04 +0700

    fix dimention btn

--- app/src/main/java/com/example/maaster/teacherassessment/AssessmentActivity.java
+++ app/src/main/java/com/example/maaster/teacherassessment/AssessmentActivity.java
@@ -130,7 +130,7 @@ public class AssessmentActivity extends AppCompatActivity {
                                     }
                                     if(k==questions.size()-2){
                                         previusIcon = (Button)findViewById(R.id.previous_icon);
-                                        previusIcon.setVisibility(View.INVISIBLE);
+                                        previusIcon.setVisibility(View.GONE);
                                     }
                                     questions.get(k).setAnswer(answers);
                                     Log.d(TAG, "onFinish: "+k);
@@ -204,7 +204,7 @@ public class AssessmentActivity extends AppCompatActivity {
         }
         if(checkpresent<k||k==questions.size()-1){
             previusIcon = (Button)findViewById(R.id.previous_icon);
-            previusIcon.setVisibility(View.INVISIBLE);
+            previusIcon.setVisibility(View.GONE);
             backfirst=0;
             clearBtn();
         }
@@ -237,8 +237,9 @@ public class AssessmentActivity extends AppCompatActivity {
 
         if(k==0) {
             backIcon = (Button) findViewById(R.id.back_icon);
-            backIcon.setVisibility(View.INVISIBLE);
-
+            backIcon.setVisibility(View.GONE);
+            Log.d(TAG, "backNo: *************");
+            
 
 
             if(backfirst!=0){
